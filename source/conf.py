0d2480afac266daa61f99db29a5910c3e19bda91
Alex Band <alex@nlnetlabs.nl>
2021-12-15 12:36:29 +0100

    Add favicon

--- source/conf.py
+++ source/conf.py
@@ -79,6 +79,7 @@ pygments_style = 'sphinx'
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_logo = 'resources/nsd-duotone-white.png'
+html_favicon = 'resources/favicon.ico'
 html_theme_options = {
     'logo_only': True,
     'display_version': True,

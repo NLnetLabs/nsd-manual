42203bd340d4d45c66442b1acb7875d59104206f
Alex Band <alex@nlnetlabs.nl>
2021-12-21 09:18:39 +0100

    add autolabel

--- source/conf.py
+++ source/conf.py
@@ -41,6 +41,7 @@ release = '4.3.9'
 extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.ifconfig',
+    'sphinx.ext.autosectionlabel'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
